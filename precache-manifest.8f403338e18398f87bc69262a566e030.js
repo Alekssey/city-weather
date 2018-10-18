53b7f37f471d5c823dbac039b279e6b992e2f9b2
Alekssey <timoschenko.aleksey@gmail.com>
2018-10-18 03:14:43 +0200

    spa city-weather

---
+++ precache-manifest.8f403338e18398f87bc69262a566e030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6e63a7ef20baa584a4f1",
+    "url": "/static/js/main.6e63a7ef.chunk.js"
+  },
+  {
+    "revision": "66b0f155e2aea623ad6f",
+    "url": "/static/js/1.66b0f155.chunk.js"
+  },
+  {
+    "revision": "6e63a7ef20baa584a4f1",
+    "url": "/static/css/main.5fa2b373.chunk.css"
+  },
+  {
+    "revision": "66b0f155e2aea623ad6f",
+    "url": "/static/css/1.6b065024.chunk.css"
+  },
+  {
+    "revision": "1dc42520b913a021bb534d9829f86c0f",
+    "url": "/index.html"
+  }
+];
